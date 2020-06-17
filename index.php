a25dd9d454693a257fe8ce7a2735302c72d83224
Aman sharma <52895888+Aman-Sharmaa@users.noreply.github.com>
2020-06-17 19:09:05 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -13,12 +13,28 @@
 	}
 
 ?>
+
+
+    <?php
+
+$con = mysqli_connect('localhost','root');
+
+mysqli_select_db($con,'registration');
+  
+ $q = "select * from users where email = 'amansharma57269@gmail.com'";
+
+ $query = mysqli_query($con,$q);
+
+ while($res = mysqli_fetch_array($query)){
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css"><link href="https://fonts.googleapis.com/css2?family=Odibee+Sans&family=Suez+One&display=swap" rel="stylesheet">
 <style>
 body {
   margin: 0;
@@ -86,6 +102,61 @@ body {
     text-align: left;
   }
 }
+
+.profile{
+  width: 350px;
+  height: 500px;
+  background-color:white;
+  border-radius: 5px;
+  color: black;
+  float: left;
+  margin-left: 40%;
+  margin-top: 40px;
+  font-family: 'Odibee Sans', cursive;
+   background-color:#212121;
+
+}
+.userprofile {
+  text-align:center;
+  border-bottom: solid white 1px;
+  padding: 10px;
+}
+.userprofile i {
+  font-size: 60px;
+  margin-bottom: 50px;
+  
+}
+.username p{
+   margin-top: 15px;
+   color: white;
+  }
+.username{
+  width: 100%;
+  height: 70px;
+  font-family: 'Suez One', serif;
+  margin-top: 30px;
+  margin-left: 10px;
+  color:white;
+
+}
+.username i{
+  margin-right: 30px;
+  float: right;
+  cursor: pointer;
+}
+.logout a{
+  width: 150px;
+  text-align:center;
+  text-decoration: none;
+ padding: 10px;
+ color: black;
+  background-color: white;
+  margin-left: 27%;
+  margin-top: 100px;
+  float: left;
+  border-radius: 5px;
+
+}
 </style>
 </head>
 <body>
@@ -100,13 +171,10 @@ body {
   <a href="#about">Event</a>
   <a href="#about">Contact Us </a>
   <a href="#about">Pay Now</a>
-  <?php  if (isset($_SESSION['username'])) : ?>
-      <a>Welcome <strong><?php echo $_SESSION['username']; ?></strong></a>
-       <a href="index.php?logout='1'" style="color: red;">logout</a> 
-    <?php endif ?>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
+ 
 </div>
 
 
@@ -122,7 +190,22 @@ function myFunction() {
 }
 </script>
 
+<div class="profile">
+  <div class="userprofile"><img src="user.webp" width="100px"></img></div>
+ 
+   <?php  if (isset($_SESSION['username'])) : ?>
+      <div class="username"><p>Username</p><?php echo $_SESSION['username']; ?></strong><i class="fa fa-edit"></i></div>
+      <div class="username"><p>Email</p><?php echo $res['email']; ?><i class="fa fa-edit"></i></div>
+    <?php endif ?>
+ 
+  
 
-		
+
+<?php  if (isset($_SESSION['username'])) : ?>
+      <div class="logout"> <a href="index.php?logout='1'" >logout</a></div>
+    <?php endif ?>
+</div>
+<?php } ?>
+
 </body>
 </html>
